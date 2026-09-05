7894a9dd88b44ebf159b5a24234842d214678af6
Sainikitha <sainikithasingireddy@gmail.com>
2026-09-05 11:06:30 +0530

    Add Next.js config for Vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  reactStrictMode: true
-}
+import path from "path";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname, ".."),
+};
+
+export default nextConfig;
